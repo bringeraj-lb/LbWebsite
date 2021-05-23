aceb445636651f7f0874b31f5321b4b0f8d46060
Pavan <pavans.shetty.2452@gmail.com>
2021-05-23 19:10:30 +0530

    update 1

---
+++ asset/js/script.js
@@ -0,0 +1,10 @@
+$(document).on("scroll", function(){
+
+    if ($(document).scrollTop() >= 80){
+        $("#navbar").addClass("main-navigation");
+    } else {
+         
+          $("#navbar").removeClass("main-navigation");
+    }
+
+});
